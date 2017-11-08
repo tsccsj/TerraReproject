76a7cd3146e0fda6335d39f8a201893e3bbc1048
tsccsj <yizhaotsccsj@gmail.com>
2017-11-08 11:18:19 -0600

    limit spatial indexing size

--- src/reproject.c
+++ src/reproject.c
@@ -1,7 +1,7 @@
 /**
  * reproject.c
  * Authors: Yizhao Gao <ygao29@illinois.edu>
- * Date: {10/13/2017}
+ * Date: {10/10/2017}
  */
 
 
@@ -258,7 +258,13 @@ void nearestNeighborBlockIndex(double ** psouLat, double ** psouLon, int nSou, d
 
 	const double earthRadius = 6367444;
 	double maxradian = maxR / earthRadius;
-	int nBlockY = M_PI / maxradian;
+
+	double blockSizeRadian = maxradian;
+	if(maxR > 1000) {
+		blockSizeRadian = 1000 / earthRadius;
+	}
+
+	int nBlockY = M_PI / blockSizeRadian;
 
 	double latBlockR = M_PI / nBlockY;
 
@@ -279,7 +285,7 @@ void nearestNeighborBlockIndex(double ** psouLat, double ** psouLon, int nSou, d
 		exit(1);
 	}
 
-	struct LonBlocks * souIndex = pointIndexOnLatLon(psouLat, psouLon, souID, nSou, nBlockY, maxradian);
+	struct LonBlocks * souIndex = pointIndexOnLatLon(psouLat, psouLon, souID, nSou, nBlockY, blockSizeRadian);
 
 	souLat = *psouLat;
 	souLon = *psouLon;
